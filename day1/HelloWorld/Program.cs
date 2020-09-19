3f2147e12acf72cc41fffa69501efbcd363f584c
muskan <mbnm2004@gmail.com>
2020-09-19 20:53:59 +0530

    day1

---
+++ day1/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name:");
+            var name=Console.ReadLine();
+            var date=DateTime.Now;
+            System.Console.WriteLine("Hello {0} on Date {1} at time {2}",name,date.ToShortDateString(),date.ToShortTimeString());
+            System.Console.WriteLine("Press any key to exit!!");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ day1/README.md
@@ -0,0 +1,6 @@
+Day1 Topics Covered :
+
+1.Create, Debug and Publish a .NET Core console application using Visual Studio Code.
+2.Create, Unit Testing a .NET Standard library using Visual Studio Code.
+3.Create and publish a package .
+4.Install and use a package using the dotnet CLI.
